c6dcbb5c22138cb882f7a5662fbbb908db3da9df
deepaksweety <deepakvicky602@gmail.com>
2023-01-02 10:57:53 +0530

    upload files

---
+++ part.js
@@ -0,0 +1,18 @@
+        //practice 1
+       /* 
+       var name = prompt('hello enter your name pls:')
+       var age = prompt('enter your age:')
+       ageday = age * 365.25 
+       alert('welcome to the new year'+name+'2023'+ageday+'enjoy it')
+       */
+      // practice 2
+      let todo1 = 'get groceries';
+      let todo2 = 'washing cars';
+      let todo3 = 'make dinner';
+      console.log(todo1);
+      console.log(todo2);
+      console.log(todo3);
+
+      let element = document.createElement('div');
+      element.innerHTML = todo1;
+      document.body.appendChild(element);
